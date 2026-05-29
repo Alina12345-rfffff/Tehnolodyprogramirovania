f3fe8af226158a7afcff46c61a20250169e8f827
Алина <aueralina880@gmail.com>
2026-05-29 23:32:09 +0700

    а

---
+++ Аренда Домиков/Domain/АрендаДомика.Domain.Repositories.Abstractions/IBookingRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace АрендаДомика.Domain.Repositories.Abstract;
+
+public interface IBookingRepository
+{
+    // Найти бронь по ID
+    Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    // Сценарий: Запрос на бронь
+    Task AddAsync(Booking booking, CancellationToken cancellationToken = default);
+
+    // Сценарий: Подтвердить/отклонить бронь
+    Task UpdateAsync(Booking booking, CancellationToken cancellationToken = default);
+
+    // Просмотр всех бронирований для конкретного домика
+    Task<IEnumerable<Booking>> GetByHouseIdAsync(Guid houseId, CancellationToken cancellationToken = default);
+}
