50df31fa76926f0939896bddcf3292dced1095ce
Алина <aueralina880@gmail.com>
2026-05-29 14:50:47 +0700

    .

---
+++ Аренда Домиков/Domain/АрендаДомика.ValueObjects/Exceptions/ValidatorNullException.cs
@@ -0,0 +1,6 @@
+﻿
+
+namespace АрендаДомика.ValueObjects.Exceptions;
+
+public class ValidatorNullException(string paramName)
+: ArgumentNullException(paramName, $"Validator \"{paramName}\" must be specified for type.");
